fc2396a5247cccfe4620803a39fd6f39d3952577
Magnus Morton <magnusmorton@icloud.com>
2017-05-24 00:43:09 +0100

    fixed it again

--- append.sh
+++ append.sh
@@ -1,3 +1,3 @@
 for f in *.rkt; do
-    echo "\n(require \"cost.rkt\")\n(define eqns (equations))\n(printf \"~a,~a,~a,~a,~a,~a~n\" (hash-ref eqns 0) (hash-ref eqns 1) (hash-ref eqns 2) (hash-ref eqns 3) (hash-ref eqns 4) (hash-ref eqns 5)) | tee -a $f"
+    echo "$(cat $f)\n(require \"cost.rkt\")\n(define eqns (equations))\n(printf \"~a,~a,~a,~a,~a,~a~n\" (hash-ref eqns 0) (hash-ref eqns 1) (hash-ref eqns 2) (hash-ref eqns 3) (hash-ref eqns 4) (hash-ref eqns 5))" | $f
 done
